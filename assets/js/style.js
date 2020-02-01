c06c770e474239ea3c450758d892eb8f29aa7749
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-02-01 12:11:11 +0000

    Test for js function iterating through array added

---
+++ assets/js/style.js
@@ -0,0 +1,14 @@
+const cards = ['a', 'b', 'c', 'd'];
+
+const cardContainer = document.getElementById('card-container');
+
+for (let i = 0; i <= cards.length - 1; i++) {
+  const cardDiv = document.createElement('div');
+  cardDiv.innerText = cards[i];
+  cardDiv.classList.add('card');
+ 
+ 
+  cardContainer.appendChild(cardDiv);
+}
+
+console.log(cardContainer);
