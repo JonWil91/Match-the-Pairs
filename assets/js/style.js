c3ab727c6a161a150d15ce4978db2cd7546485ed
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-02-08 14:42:48 +0000

    Pre commit before changes are made to body layout

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,18 +1,23 @@
-  /* const cards = ['a', 'b', 'c', 'd'];
+const cardss = ['a', 'b', 'c', 'd'];
 
-const cardContainer = document.getElementById('card-container');
+const cardContainerr = document.getElementById('card-containerr');
 
-for (let i = 0; i <= cards.length - 1; i++) {
+for (let i = 0; i <= cardss.length - 1; i++) {
   const cardDiv = document.createElement('div');
-  cardDiv.innerText = cards[i];
+  cardDiv.innerText = cardss[i];
   cardDiv.classList.add('card');
  
  
-  cardContainer.appendChild(cardDiv);
+  cardContainerr.appendChild(cardDiv);
 }
 
-console.log(cardContainer); */
+console.log(cardContainer); 
 
+// https://www.w3schools.com/js/js_objects.asp
+// https://www.w3schools.com/jsref/prop_img_src.asp
+
+
+// Inspiration for flipCard function https://www.youtube.com/watch?v=IMz_68rfhXA&list=PLLX1I3KXZ-YH-woTgiCfONMya39-Ty8qw&index=6 //
 
 const cards = document.querySelectorAll(".card-container");
 
@@ -21,3 +26,10 @@ function flipCard() {
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+const charmander = {
+    id: 'card 1',
+    image: { src : "assets/images/charmander.png" }
+}
+
+console.log(charmander.image);
