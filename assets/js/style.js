36923b7b4c9d5bac87ddda640aada94ac57b2f8b
Jon Williams <jrwilliams100@hotmail.co.uk>
2020-02-08 14:56:08 +0000

    Dynamic layout of card classes

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,35 +1,13 @@
-const cardss = ['a', 'b', 'c', 'd'];
+const cards = ['a', 'b', 'c', 'd'];
 
-const cardContainerr = document.getElementById('card-containerr');
+const cardContainer = document.getElementById('card-container');
 
-for (let i = 0; i <= cardss.length - 1; i++) {
+for (let i = 0; i <= cards.length - 1; i++) {
   const cardDiv = document.createElement('div');
-  cardDiv.innerText = cardss[i];
+  cardDiv.innerText = cards[i];
   cardDiv.classList.add('card');
- 
- 
-  cardContainerr.appendChild(cardDiv);
+  cardDiv.classList.add('card-back');
+  cardContainer.appendChild(cardDiv);
 }
 
-console.log(cardContainer); 
-
-// https://www.w3schools.com/js/js_objects.asp
-// https://www.w3schools.com/jsref/prop_img_src.asp
-
-
-// Inspiration for flipCard function https://www.youtube.com/watch?v=IMz_68rfhXA&list=PLLX1I3KXZ-YH-woTgiCfONMya39-Ty8qw&index=6 //
-
-const cards = document.querySelectorAll(".card-container");
-
-function flipCard() {
-    this.classList.toggle('flip');
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-const charmander = {
-    id: 'card 1',
-    image: { src : "assets/images/charmander.png" }
-}
-
-console.log(charmander.image);
+console.log(cardContainer);
